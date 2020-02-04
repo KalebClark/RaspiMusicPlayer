341afe8c3b3bd1b9f91e54d459c2313589202bf5
Kaleb Clark <kaleb@abraxxus.net>
2020-02-04 23:53:36 +0000

    All buttons & Switches working

--- RaspiMusic/RaspiMusic.py
+++ RaspiMusic/RaspiMusic.py
@@ -24,6 +24,7 @@ class RaspiMusic:
         self.vol_raw_range = [0, 65472]
         self.vol_real_range = [100, 0]
         self.bouncetime = 500
+        self.dim_amount = 50
         self.vstate = {
             'status': None,
             'seek': 0,
@@ -55,10 +56,10 @@ class RaspiMusic:
         GPIO.setup(self.pin_in2, GPIO.OUT)
         GPIO.setup(self.pin_pwm, GPIO.OUT)
         GPIO.setup(self.pin_en, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pin_pwm, 1000)
-        self.pwm.start(50)
+        self.pwm = GPIO.PWM(self.pin_pwm, 2000)
+        self.pwm.start(45)
         GPIO.output(self.pin_en, GPIO.HIGH)
-        self.motorSet(60)
+        #self.motorSet(53)
         self.motorOff()
 
         GPIO.setmode(GPIO.BCM)
@@ -77,26 +78,47 @@ class RaspiMusic:
         GPIO.add_event_detect(5, GPIO.FALLING, callback=self.btn_next, bouncetime=self.bouncetime)
         GPIO.add_event_detect(6, GPIO.FALLING, callback=self.btn_random, bouncetime=self.bouncetime)
 
-    def tgl_dim(self, channel):
-        print("Dim")
+    def tgl_dim(self):
+        cur_dim = self.vstate['dim']
+        if cur_dim:
+            self.vstate['dim'] = False
+            new_vol = self.getVolumeKnob()
+        else:
+            self.vstate['dim'] = True
+            new_vol = self.getDimAmount()
 
-    def tgl_mute(self, channel):
-        print("Mute")
+        self.sendApiCmd("volume", str(int(new_vol)))
+
+    def tgl_mute(self):
+        cur_mute = self.vstate['mute']
+        if cur_mute:
+            mute_cmd = 'unmute'
+            self.vstate['mute'] = False
+        else:
+            mute_cmd = 'mute'
+            self.vstate['mute'] = True
+
+        self.sendApiCmd("volume", mute_cmd)
 
     def btn_repeat(self, channel):
         print("Repeat")
+        self.sendApiCmd("repeat", True)
 
     def btn_prev(self, channel):
         print("Previous")
+        self.sendApiCmd("prev")
 
     def btn_playpause(self, channel):
         print("Play / Pause")
+        self.sendApiCmd("toggle")
 
     def btn_next(self, channel):
         print("Next")
+        self.sendApiCmd("next")
 
     def btn_random(self, channel):
         print("Random")
+        self.sendApiCmd("random", True)
 
     def motorLeft(self):
         GPIO.output(self.pin_in1, False)
@@ -137,11 +159,22 @@ class RaspiMusic:
             api_url = api_url + "&" + cmd + "=" + str(arg)
 
         resp = requests.get(api_url)
-        if resp.status_code == 200:
+        if resp.status_code != 200:
             print("Error!!! This didnt happen: ", api_url)
 
         logging.info("sendApiCmd: {} {}".format(api_url, resp.status_code))
         return resp
+
+    def setVolume(self, volume):
+        if self.vstate['mute']:
+            return
+        if self.vstate['dim']:
+            volume = self.getDimAmount()
+        self.sendApiCmd("volume", volume)
+    
+    def getDimAmount(self):
+        cur_vol = self.getVolumeKnob()
+        return cur_vol - ((self.dim_amount * cur_vol) / 100)
 
     def getFaderPos(self):
         fad_raw = AnalogIn(self.mcp, MCP.P1).value
@@ -195,26 +228,82 @@ class RaspiMusic:
         self.vstate['mute'] = data['mute']        
         #print(state.json())
 
+    def getMuteSwitch(self):
+        if GPIO.input(16) == GPIO.HIGH:
+            return True
+        else:
+            return False
+
+    def getDimSwitch(self):
+        if GPIO.input(26) == GPIO.HIGH:
+            return True
+        else:
+            return False
+
     def update(self):
         # Dim & Mute
-        if GPIO.input(26) == GPIO.HIGH:
-            print("Dim")
-        if GPIO.input(16) == GPIO.HIGH:
-            print("Mute")
+        mute_switch = self.getMuteSwitch()
+        dim_switch = self.getDimSwitch()
+
+        if mute_switch != self.vstate['mute']:
+            self.tgl_mute()
+
+        if dim_switch != self.vstate['dim']:
+            print("FUUUUUCK DIM!")
+            self.tgl_dim()
 
         # Volume
-        vol = self.getFaderPos()
-        print(vol)
+        vol = self.getVolumeKnob()
+        if vol != self.vstate['volume']:
+            if not self.vstate['dim']:
+                self.setVolume(vol)
+
+        # Get Fader Position
+        fader_pos = self.getFaderPos()
 
         # Get song Position
         song_pos = self.getSongPos()
-        print(song_pos)
 
         # Turn off motor controller when touched.
         if self.fadeTouch():
             GPIO.output(self.pin_en, GPIO.LOW)
         else:
             GPIO.output(self.pin_en, GPIO.HIGH)
+
+        diff = abs(song_pos - fader_pos)        
+        
+        # Set Speed
+        if diff >= 10:
+            self.pwm.ChangeDutyCycle(90)
+        else:
+            self.pwm.ChangeDutyCycle(50)
+
+        #if diff <= 10:
+        
+        print("vol: {%d/%d}, mute: {%r/%r}, dim: {%r/%r}, song_pos: {%d}, fader_pos: {%d} Difference: {%d}" % (
+            vol,
+            self.vstate['volume'],
+            mute_switch,
+            self.vstate['mute'],
+            dim_switch,
+            self.vstate['dim'],
+            song_pos,
+            fader_pos,
+            diff
+        ))
+
+        return 
+        # Set Speed 
+        if abs(song_pos - fader_pos) >= 10:
+            self.pwm.ChangeDutyCycle(90)
+        else:
+            self.pwm.ChangeDutyCycle(55)
+
+        # Move the fader
+        if song_pos >= fader_pos:
+            self.motorRight()
+        elif song_pos <= fader_pos:
+            self.motorLeft()
 
         #self.motorLeft()
         #self.motorOff()
